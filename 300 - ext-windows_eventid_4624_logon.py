0ee0ccafcb6f272ea3e077f432996ffccce554dd
alchy <alchy@users.noreply.github.com>
2019-10-07 16:11:11 +0200

    Update 300 - ext-windows_eventid_4624_logon.py

--- 300 - ext-windows_eventid_4624_logon.py
+++ 300 - ext-windows_eventid_4624_logon.py
@@ -18,7 +18,7 @@ from elasticsearch import Elasticsearch
 DEBUG           = True
 ES_USER         = 'python'
 ES_PASS         = '3507bcd4bd8655c1731265f2f4d7272b'
-ES_SIZE         = 1023
+ES_SIZE         = 1
 ES_TIMEOUT      = 1024
 
 
@@ -34,38 +34,36 @@ class GracefulKiller:
 
 def processing(hit):
   if DEBUG:
-    print("[d] prociessing function called...")
-  ext_index = hit['_index']
-  ext_id = hit['_id']
-  ext = {}
+    print("[d] processing function called...")
 
-  ext['@timestamp' ] = hit['_source']['@timestamp']
-  ext['origin'] = hit['_source']['winlog']['computer_name'].lower()
+  ext = {}
+  ext['@timestamp' ] = hit['@timestamp']
+  ext['origin'] = hit['winlog']['computer_name'].lower()
   ext['action'] = "logon"
 
   ''' include original message before we update with ours '''
-  ext['message'] = hit['_source']['message']
+  ext['message'] = hit['message']
 
   ''' make a single detail array from the specific attributes '''
   ''' get logontype '''
   try:
-    if hit['_source']['winlog']['event_data']['LogonType'] == 2:
+    if hit['winlog']['event_data']['LogonType'] == 2:
       ext['logon_type'] = "interactive"
-    elif hit['_source']['winlog']['event_data']['LogonType'] == 3:
+    elif hit['winlog']['event_data']['LogonType'] == 3:
       ext['logon_type'] = "network"
-    elif hit['_source']['winlog']['event_data']['LogonType'] == 4:
+    elif hit['winlog']['event_data']['LogonType'] == 4:
       ext['logon_type'] = "batch"
-    elif hit['_source']['winlog']['event_data']['LogonType'] == 5:
+    elif hit['winlog']['event_data']['LogonType'] == 5:
       ext['logon_type'] = "service"
-    elif hit['_source']['winlog']['event_data']['LogonType'] == 7:
+    elif hit['winlog']['event_data']['LogonType'] == 7:
       ext['logon_type'] = "unlock"
-    elif hit['_source']['winlog']['event_data']['LogonType'] == 8:
+    elif hit['winlog']['event_data']['LogonType'] == 8:
       ext['logon_type'] = "cleartext"
-    elif hit['_source']['winlog']['event_data']['LogonType'] == 9:
+    elif hit['winlog']['event_data']['LogonType'] == 9:
       ext['logon_type'] = "run as"
-    elif hit['_source']['winlog']['event_data']['LogonType'] == 10:
+    elif hit['winlog']['event_data']['LogonType'] == 10:
       ext['logon_type'] = "remote interactive"
-    elif hit['_source']['winlog']['event_data']['LogonType'] == 11:
+    elif hit['winlog']['event_data']['LogonType'] == 11:
       ext['logon_type'] = "cached interactive"
     else:
       ext['logon_type'] = "unknown"
@@ -76,20 +74,21 @@ def processing(hit):
 
   ''' get status '''
   ext['status'] = "unknown"
-  if "Audit Success" in hit['_source']['winlog']['keywords']:
+  if "Audit Success" in hit['winlog']['keywords']:
     ext['status'] = "passed"
-  if "Audit Failure" in hit['_source']['winlog']['keywords']:
+  if "Audit Failure" in hit['winlog']['keywords']:
     ext['status'] = "failed"
 
   ''' get user '''
   try:
-    ext['src_user'] = hit['_source']['winlog']['event_data']['TargetUserName'].lower()
+    ext['src_user'] = hit['winlog']['event_data']['TargetUserName'].lower()
   except KeyError:
     ext['src_user'] = "unknown"
 
   ''' get src (src is where the req. is coming from)'''
+  ''' convert the ipaddress to hostname '''
   try:
-    ext['src_ip'] = hit['_source']['winlog']['event_data']['IpAddress']                                 # get ip address
+    ext['src_ip'] = hit['winlog']['event_data']['IpAddress']                                    # get ip address
     socket.inet_aton(ext['src_ip'])                                                                     # check if it is ipv4 \
     ext['src_fqdn'] = str(resolver.query(reversename.from_address(ext['src_ip']),"PTR")[0])[:-1]        # make src_ip fqdn    |
   except KeyError:
@@ -102,18 +101,18 @@ def processing(hit):
   ''' get dst (src is where the req. is coming for)'''
   ''' convert the hostname to ip address '''
   try:
-    ext['dst_fqdn'] = hit['_source']['winlog']['computer_name'].lower()                                 # convert name to lowercase
+    ext['dst_fqdn'] = hit['winlog']['computer_name'].lower()                                    # convert name to lowercase
     ext['dst_ip'] = socket.gethostbyname(ext['dst_fqdn'])                                               # make it ipv4
   except socket.gaierror:                                                                               # reverese can't be made
     ext['dst_ip'] = "0.0.0.1"                                                                           # failed, unresolvable, assign something
   ''' convert the ip address to hostname '''
   try:
-    ext['src_fqdn'] = str(resolver.query(reversename.from_address(ext['src_ip']),"PTR")[0])[:-1]        # make src_ip fqdn, otherwise suffix loss
+    ext['dst_fqdn'] = str(resolver.query(reversename.from_address(ext['dst_ip']),"PTR")[0])[:-1]        # make src_ip fqdn, otherwise suffix loss
   except resolver.NXDOMAIN:                                                                             # reverse resolve impossible
-    ext['src_fqdn'] = "unresolvable.somedomain"                                                         # failed, unresolvable, assign something
+    ext['dst_fqdn'] = "unresolvable.somedomain"                                                         # failed, unresolvable, assign something
 
   ext['parsed'] = 'true'
-  return(ext_index, ext_id, json.dumps(ext, indent = 2))
+  return(json.dumps(ext, indent = 2))
 
 
 if __name__ == "__main__":
@@ -143,25 +142,21 @@ if __name__ == "__main__":
                             request_timeout = ES_TIMEOUT )
 
       if DEBUG:
-        print("[d] elasticsearchi responded, processing results...")
+        print("[d] elasticsearch responded, processing results...")
 
       for hit in response['hits']['hits']:
         if DEBUG:
           print(json.dumps(hit, indent = 2))
 
-        ext_index, ext_id, ext = processing(hit)
-
         if DEBUG:
             print("[d] query processed and saved into elasticsearch...")
-            print("[d] ID:    ", ext_id)
-            print("[d] INDEX: ", ext_index)
-            print(ext)
+            print("[d] ID:    ", hit['_id'])
+            print("[d] INDEX: ", hit['_index'])
 
-        res = es.index( index = ext_index, id = ext_id, body = ext )
+        res = es.index( index = hit['_index'], id = hit['_id'], body = processing(hit['_source']) )
         if DEBUG:
           print(res['result'])
 
     print("End of the program. I was killed gracefully :)")
   except Exception as e:
     print("Error while parsing. Program exit :(", str(e), json.dumps(response, indent = 2))
-
