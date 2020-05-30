55ed55e5c1858ddcf8ad9fd0e125e4d512d784f9
alchy <alchy@users.noreply.github.com>
2020-05-30 09:48:22 +0200

    Update kav_agent_helper.php

--- kav_agent_helper.php
+++ kav_agent_helper.php
@@ -6,8 +6,11 @@ $out = fopen($argv[2], "w+");
 
 $start = microtime(true);
 
+$lines_in = 0;
 while(($line = fgets($in)) !== false) {
-    $converted = iconv("cp1250", "utf-8", $line);
+    $converted = iconv("cp1250", "ASCII//TRANSLIT", $line);
+    #$converted = iconv("cp1250", "US-ASCII", $line);
+    $lines_in = $lines_in + 1;
     fwrite($out, $converted);
 }
 
@@ -22,19 +25,28 @@ while($line = fgets($in)) {
 fclose($out);
 fclose($in);
 
+
 $in = fopen($argv[1], "r");
 $out = fopen($argv[2], "w+");
 while($line = fgets($in)) {
 	$line = str_replace("wstrPar9;", "wstrPar9\n", $line);
-	$line = str_replace("-202", "\n202", $line);
-	$line = str_replace(";202", "\n202", $line);
+	$line = str_replace("-20", "\n20", $line);
+	$line = str_replace(";20", "\n20", $line);
   	fwrite($out, $line);
 }
 fclose($out);
 fclose($in);
 
+$lines_out = 0;
+$out = fopen($argv[2], "r");
+while($line = fgets($out)) {
+	$lines_out = $lines_out + 1;
+}
+
 $elapsed = microtime(true) - $start;
-echo "\r\n", "[d] PHP: input file:  ", $argv[1], "\r\n";
-echo "\r\n", "[d] PHP: output file: ", $argv[2], "\r\n";
-echo "\r\n", "[d] PHP: iconv took $elapsed seconds\r\n";
+echo "[d] PHP: input file:  ", $argv[1];
+echo "[d] PHP: output file: ", $argv[2];
+echo "[d] PHP: iconv took $elapsed seconds";
+echo "[d] PHP: lines_in:  " , $lines_in;
+echo "[d] PHP: lines_out: " , $lines_out;
 ?>
