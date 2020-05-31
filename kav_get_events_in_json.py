d2e23402403f21f7bb7d182402b15a729af2d7b5
alchy <alchy@users.noreply.github.com>
2020-06-01 00:19:09 +0200

    Update kav_get_events_in_json.py

--- kav_get_events_in_json.py
+++ kav_get_events_in_json.py
@@ -6,17 +6,18 @@ import json
 import time
 import ctypes
 import locale
-import datetime
 import subprocess
 from shutil import copyfile
+from datetime import datetime
 
-sql_user            = "*****"
-sql_pass            = "*****"
+
+sql_user            = "qradar"
+sql_pass            = "qradar2018!"
 sql_dump            = "C:\\Users\\obitadmin\\Elasticsearch\\dump\\kav_events.dump"
 sql_dump_debug      = "C:\\Users\\obitadmin\\Elasticsearch\\dump\\kav_events.dump.debug"
 sql_dump_converted  = "C:\\Users\\obitadmin\\Elasticsearch\\dump\\kav_events.dump.tmp"
 sql_json            = "C:\\Users\\obitadmin\\Elasticsearch\\json\\kav_events-" + \
-                      datetime.datetime.now().strftime('%Y%m%d%H%M%S') + ".json" 
+                      datetime.now().strftime('%Y%m%d%H%M%S') + ".json" 
 
 enforce_encoding = True
 
@@ -35,7 +36,6 @@ cmd    = [
            '-o', sql_dump,
            '-s', '|',
            '-W',
-           #'-f', "65001",
            '-Q', sql_query
           ]
 print("[d] reading database")
@@ -73,10 +73,27 @@ with io.open(sql_json,'wt', encoding = dst_file_encoding) as json_out:
       if line_nr > 1:
         row['src_type'] = 'kaspersky'
         try:
-          row['tmRiseTime'] = str(datetime.datetime.strptime(row['tmRiseTime'], "%Y-%m-%d %H:%M:%S.%f").isoformat())
+          # https://www.elastic.co/guide/en/elasticsearch/reference/current/mapping-date-format.html#strict-date-time
+          # basic_date_time_no_millis
+          # A basic formatter that combines a basic date and time without millis, separated by a T: yyyyMMdd'T'HHmmssZ.
+          m, s = divmod(time.timezone, 60)
+          h, m = divmod(m, 60)
+          time_offset = "%.2d%.2d" % (h, m)
+          row['tmRiseTime'] = datetime.strptime(row['tmRiseTime'], '%Y-%m-%d %H:%M:%S.%f')
+          row['tmRiseTime'] = row['tmRiseTime'].strftime("%Y%m%dT%H%M%S") + time_offset
+          row['tmRegistrationTime'] = datetime.strptime(row['tmRegistrationTime'], '%Y-%m-%d %H:%M:%S.%f')
+          row['tmRegistrationTime'] = row['tmRegistrationTime'].strftime("%Y%m%dT%H%M%S") + time_offset
+
+          if "GNRL_EV_VIRUS_FOUND_AND_BLOCKED" in row['strEventType']:
+            domain, user = row['wstrPar7'].split('\\')
+            row['user'] = {"name": user, "domain": domain }
+            row['message'] = row['wstrEventTypeDisplayName']
+            row['event'] = {"name": "Malware", "action": "Blocked"}
+            print(row)
           converted = json.dumps(row, sort_keys=True)+"\n"
           json_out.write(converted)  
         except:
-          print('[d] exception: ', row)
+          pass
+          #print('[d] exception: ', row)
       line_nr += 1
     print("[d] total lines: ", line_nr)
